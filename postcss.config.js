e24ef6ef0e3cca4e7e00567d22e80c7390e60070
Ramiro Azar <ramiro@typeandpixel.com.au>
2022-09-07 10:13:14 +1000

    feat: remove tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,8 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
     'postcss-preset-env': {
-      features: {'nesting-rules': false},
+      features: { 'nesting-rules': false },
     },
   },
 };
